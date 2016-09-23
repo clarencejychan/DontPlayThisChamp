fd66f41da23bf82ef42e8ff844deaa694c7be9ee
Clarence Chan <cchan@metroland.com>
2016-09-23 17:18:34 -0400

    Updated some Rito API stuff + Initial AJAX calls

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import json
+import requests
+
+
 
 app = Flask(__name__)
 
@@ -7,6 +11,34 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html');
 
+
+@app.route("/searchhandler", methods=['GET', 'POST'])
+def search():
+
+	riotKey = 'RGAPI-33ddda68-2769-46cc-8a03-1c8c393551ad'
+
+	if request.method == 'POST':
+		searchName = request.get_data().decode("utf-8")
+		
+		summInfoJson = requests.get('https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/' + searchName + '?api_key=' + riotKey)
+		summInfo = json.loads(summInfoJson.text)
+		summInfoId = summInfo[searchName]['id']
+		print(summInfoId)
+		summChampInfoJson = requests.get('https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/' + summInfoId + '/ranked?season=SEASON2016&api_key=' + riotKey)
+
+
+		'''
+			Next Steps:
+				Finish getting information
+				sort out the information to top3 champions
+				make some sort of js library to get the right picture depending on the champion id
+				integrate vue.js to load the correct champions depending on who it is, and to crate the right statistics (maybe just show the champions plus name is
+				good enough in some form of a lightbox)
+		'''
+
+
+
+		return str(summInfoId)
 
 
 if __name__ == "__main__":
--- static/js/main.js
+++ static/js/main.js
@@ -2,4 +2,33 @@ $(document).ready(function() {
 	document.getElementById('top-container-vid').addEventListener('loadedmetadata', function(){
 	  this.currentTime = 10;
 	}, false);
-}); 
+
+
+	$('#submit-bttn').click(function() {
+		sendit();
+		$('#sumInput').val('');
+	})
+
+	$('#sumInput').keypress(function(e){
+		if (e.which == 13) {
+			sendit();
+			$('#sumInput').val('');
+		}
+	})
+
+});
+
+function sendit() {
+	var summName = $('#sumInput').val();
+	console.log(summName);
+	$.ajax({
+		type: "POST",
+		dataType: "text",
+		data: summName	,
+		url: "http://0.0.0.0:3000/searchhandler",
+		success: function(e) {
+			console.log(e);
+		}
+	})
+
+}
